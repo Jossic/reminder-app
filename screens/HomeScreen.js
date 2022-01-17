abffed556710b6039feee4b7aa153c1370885896
Jossic <jossic.lapierre@gmail.com>
2022-01-17 15:37:46 +0100

    Page home + page projects done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	Image,
+	Platform,
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import moment from 'moment';
+import 'moment/locale/fr';
+import { LinearGradient } from 'expo-linear-gradient';
+import Colors from '../constants/Colors';
 
 const HomeScreen = () => {
+	// Variables
+	const date = moment().format('LL');
 	return (
 		<View style={styles.container}>
-			<Text>Home</Text>
+			<SafeAreaView style={{ flex: 1 }}>
+				<Text style={styles.date}>{date}</Text>
+				<View style={styles.cards}>
+					<LinearGradient
+						colors={['#ED89AF', '#F45384']}
+						style={styles.card}>
+						<Text style={styles.cardNumber}>0</Text>
+						<Text style={styles.cardText}>Notes</Text>
+					</LinearGradient>
+					<LinearGradient
+						colors={['#FED3A0', '#FFA63E']}
+						style={styles.card}>
+						<Text style={styles.cardNumber}>0</Text>
+						<Text style={styles.cardText}>Projets</Text>
+					</LinearGradient>
+				</View>
+				<Text style={styles.title}>Notes (0)</Text>
+
+				<Image
+					source={require('../assets/empty.png')}
+					style={styles.image}
+				/>
+				<Text>
+					Commencez par créer votre premier projet pour ajouter votre
+					première note par la suite.
+				</Text>
+				<TouchableOpacity activeOpacity={0.8}>
+					<LinearGradient
+						colors={['#A996F2', '#8F79FC']}
+						style={styles.addButton}>
+						<Text style={styles.addButtonText}>
+							Voir mes projets
+						</Text>
+					</LinearGradient>
+				</TouchableOpacity>
+			</SafeAreaView>
 		</View>
 	);
 };
@@ -14,7 +63,51 @@ export default HomeScreen;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: Colors.secondary,
+		// alignItems: 'center',
+		// justifyContent: 'center',
+		paddingHorizontal: 25,
+	},
+	date: {
+		fontSize: 30,
+		fontWeight: 'bold',
+		marginBottom: 30,
+		marginTop: Platform.OS === 'android' ? 50 : 30,
+	},
+	cards: {
+		flexDirection: 'row',
 		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: 'space-between',
+	},
+	card: {
+		width: '47%',
+		height: 150,
+		padding: 15,
+		marginTop: 15,
+		borderRadius: 10,
+		alignItems: 'flex-end',
+		justifyContent: 'flex-end',
+	},
+	cardNumber: { fontSize: 50, color: 'white' },
+	cardText: { color: 'white' },
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		marginTop: 45,
+		marginBottom: 15,
+	},
+	image: {
+		width: 350,
+		height: 200,
+	},
+	addButton: {
+		padding: 10,
+		borderRadius: 5,
+		marginTop: 30,
+		alignItems: 'center',
+	},
+	addButtonText: {
+		color: 'white',
+		fontSize: 18,
 	},
 });
--- screens/ProjectsScreen.js
+++ screens/ProjectsScreen.js
@@ -1,10 +1,38 @@
+import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+	Image,
+	Platform,
+	SafeAreaView,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
+import Colors from '../constants/Colors';
 
 const ProjectsScreen = () => {
 	return (
 		<View style={styles.container}>
-			<Text>Projects</Text>
+			<SafeAreaView style={{ flex: 1 }}>
+				<Text style={styles.title}>Projets</Text>
+				<View style={styles.emptyProject}>
+					<Image
+						source={require('../assets/folder.png')}
+						style={styles.image}
+					/>
+					<Text>Commencez par créer votre premier projet.</Text>
+					<TouchableOpacity activeOpacity={0.8}>
+						<LinearGradient
+							colors={['#A996F2', '#8F79FC']}
+							style={styles.addButton}>
+							<Text style={styles.addButtonText}>
+								Créer un projet
+							</Text>
+						</LinearGradient>
+					</TouchableOpacity>
+				</View>
+			</SafeAreaView>
 		</View>
 	);
 };
@@ -14,7 +42,33 @@ export default ProjectsScreen;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: Colors.secondary,
+		paddingHorizontal: 25,
+	},
+	title: {
+		fontSize: 30,
+		fontWeight: 'bold',
+		marginBottom: 30,
+		marginTop: Platform.OS === 'android' ? 50 : 30,
+	},
+	image: {
+		width: 150,
+		height: 150,
+		marginBottom: 15,
+	},
+	addButton: {
+		padding: 10,
+		borderRadius: 5,
+		marginTop: 30,
 		alignItems: 'center',
+	},
+	addButtonText: {
+		color: 'white',
+		fontSize: 18,
+	},
+	emptyProject: {
+		flex: 1,
 		justifyContent: 'center',
+		alignItems: 'center',
 	},
 });
