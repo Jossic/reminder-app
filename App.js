241d4b89c4b30cb2638a40ed32872b00e83b4055
Jossic <jossic.lapierre@gmail.com>
2022-01-20 11:25:05 +0100

    Ask permission to send notifications

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+// Librairies
+import { useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
+
 // Redux
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -10,6 +14,20 @@ import { AppNavigator } from './navigation/AppNavigator';
 const store = createStore(globalReducer, applyMiddleware(thunk));
 
 export default function App() {
+	useEffect(() => {
+		const getPermissions = async () => {
+			const { status } = await Notifications.getPermissionsAsync();
+
+			let finalStatus = status;
+
+			if (status !== 'granted') {
+				const { requestStatus } =
+					await Notifications.requestPermissionsAsync();
+				finalStatus = requestStatus;
+			}
+		};
+		getPermissions();
+	}, []);
 	return (
 		<Provider store={store}>
 			<AppNavigator />
